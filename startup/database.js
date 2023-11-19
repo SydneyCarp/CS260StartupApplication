ed41d1b1c28c9bac3b24abb71dad73461bfbb2b8
SydneyCarp <96961638+SydneyCarp@users.noreply.github.com>
2023-11-18 22:24:56 -0700

    Update database.js

--- startup/database.js
+++ startup/database.js
@@ -1,2 +1,31 @@
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://cs260Carpenter:cs2602023!@cluster0.4du7jsi.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(url);
+const db = client.db('simon');
+const scoreCollection = db.collection('score');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+async function addIdea(idea) {
+  const result = await ideaCollection.insertOne(idea);
+  return result;
+}
+
+function getIdeas() {
+  const query = { idea: { $gt: 0, $lt: 900 } };
+  const options = {
+    limit: 100,
+  };
+  const cursor = ideaCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = { addIdea, getIdeas };
